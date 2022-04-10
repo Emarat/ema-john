beb426cdfc225f00da3cb77941e1cebc9259300b
Emarat <emarathossain00@gmail.com>
2022-04-11 03:18:24 +0600

    requireAuth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Components/Header/Header';
 import Inventory from './Components/Inventory/Inventory';
 import Login from './Components/Login/Login';
 import Orders from './Components/Orders/Orders';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 import Shop from './Components/Shop/Shop';
 import SignUp from './Components/SignUp/SignUp';
 
@@ -15,7 +16,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
-        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
--- src/__auth-steps.js
+++ src/__auth-steps.js
@@ -1,8 +1,14 @@
 /** 
- * 1. create a new project in console.google.com 
+ * 1. create a new project in console.firebase.google.com 
  * 2. npm install firebase
  * 3. create firebase.init.js & import getAuth , then export auth.
  * 4. firebase setting > authentication > enable email/password auth.
+ * 5. create login/signup component & setup route
+ * 6. attach form field handler & form submit handler
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react firebasehooks
+ * 9. if user is redirected to the expected page
+ * 10. useSignInWithEmailAndPassword for signin
 */
 
 /** 
